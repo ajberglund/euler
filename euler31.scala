487399ffee7f332cc8b15e308afde31bf08a9d26
Andrew Berglund <berglund@quantifind.com>
2013-12-22 10:05:37 -0800

    Euler 31

---
+++ euler31.scala
@@ -0,0 +1,8 @@
+def numWays(amt: Int, coins: List[Int]): Int = {
+  if (amt == 0) 1
+  else if (amt < 0  || coins.isEmpty) 0
+  else numWays(amt - coins.head, coins) + numWays(amt, coins.tail)
+}
+
+println(numWays(200, List(1, 2, 5, 10, 20, 50, 100, 200)))
+
