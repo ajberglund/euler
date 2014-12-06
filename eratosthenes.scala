69a4b2f571a351d6657064999ace69d9c49950af
Andrew Berglund <ajberglund@gmail.com>
2014-12-06 12:48:00 -0800

    comments

--- eratosthenes.scala
+++ eratosthenes.scala
@@ -6,26 +6,33 @@ import collection.mutable.Buffer
 object Sieve {
   def apply(N: Int): Array[Int] = {
     val primes = collection.mutable.Buffer[Int]()
-    // in this array, k is prime if candidates(k-2) is true
-    // initially, all elements are candidate primes
+
+    // initially, all elements are candidate primes, so maybeNewPrime(p) = true for all p
     val maybeNewPrime = Array.fill(N)(true)
+
+    // initialize
     maybeNewPrime(0) = false
     maybeNewPrime(1) = false
 
+    // start sieve here
     var p = 2
 
+    // while any candidate primes remain
     while (p < N){
       primes += p
 
       // eliminate p and all its multiples from future contention
+      // i.e. set maybeNewPrime to false
       var strike = p
       while(strike < N) {
         maybeNewPrime(strike) = false
         strike += p
       }
+
       // now get the first remaining candidate
       while (p < N && !maybeNewPrime(p)){p += 1}
     }
+
     primes.toArray
   }
 }
